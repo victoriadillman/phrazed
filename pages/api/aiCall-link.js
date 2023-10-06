5213508d3cdad61b13c88fa04ca4a5fadede2fe2
victoriadillman <victoria.dillman@gmail.com>
2023-10-06 15:33:22 -0400

    updated ai response

--- pages/api/aiCall-link.js
+++ pages/api/aiCall-link.js
@@ -5,12 +5,16 @@ const openai = new OpenAI({
 });
 
 export default async function handler(req, res) {
-    // const completion = await openai.chat.completions.create({
-    //   messages: [{role: "user", content: "Tell me a complete idiom said in the English language."}],
-    //   model: "gpt-3.5-turbo",
-    //   temperature: 1,
-    //   max_tokens: 10
-    // });
-    const completion = {'choices':[{'message': {'content':'dummy data'}}]};
+    const completion = await openai.chat.completions.create({
+      messages: [{role: "user", content: "Tell me a complete short idiom said in the English language"}],
+      model: "gpt-3.5-turbo",
+      temperature: 1
+    });
+    const string = completion.choices[0]['message']['content'];
+    if (string[0] === '"' && string[string.length - 1] === '"') {
+      const newStr = string.slice(1, string.length - 2);
+      completion.choices[0]['message']['content'] = newStr;
+    }
+    // const completion = {'choices':[{'message': {'content':'dummy data'}}]};
     res.status(200).json(completion.choices[0]['message']['content']);
 }
